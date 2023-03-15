9006213838ced9b2fa7f3af856e91daf67f82239
Ulysse <u.hischke@outlook.fr>
2023-03-15 13:02:48 +0100

    add url api

--- src/funtion/fetchData.js
+++ src/funtion/fetchData.js
@@ -1,5 +1,5 @@
 const fetchData = async (url) => {
-    const rawData = await fetch('http://localhost:3000/api/' + url);
+    const rawData = await fetch('https://booki-backend-v2.vercel.app/' + url);
     return await rawData.json()
 }
 
